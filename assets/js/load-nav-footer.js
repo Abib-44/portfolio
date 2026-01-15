bb298d49444fe7b578288f6c4de77eca4d2de918
el <eh.benaboud.dev@gmail.com>
2026-01-15 22:01:08 +0100

    d

--- assets/js/load-nav-footer.js
+++ assets/js/load-nav-footer.js
@@ -1,43 +1,50 @@
-function loadPartial(id, path) {
-  fetch(path)
-    .then(r => {
-      if (!r.ok) throw new Error(`Errore nel caricamento: ${r.status}`);
-      return r.text();
-    })
-    .then(html => {
-      const doc = new DOMParser().parseFromString(html, 'text/html');
-      const placeholder = document.querySelector(`#${id}`);
-      if (!placeholder) return;
-      doc.body.childNodes.forEach(node => placeholder.append(node));
-    })
-    .catch(err => console.error(err));
-}
+// function loadPartial(id, path) {
+//   fetch(path)
+//     .then(r => {
+//       if (!r.ok) throw new Error(`Errore nel caricamento: ${r.status}`);
+//       return r.text();
+//     })
+//     .then(html => {
+//       const doc = new DOMParser().parseFromString(html, 'text/html');
+//       const placeholder = document.querySelector(`#${id}`);
+//       if (!placeholder) return;
+//       doc.body.childNodes.forEach(node => placeholder.append(node));
+//     })
+//     .catch(err => console.error(err));
+// }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const currentUrl = new URL(window.location.href);
-  let navPath, footerPath;
+// document.addEventListener('DOMContentLoaded', () => {
+//   const currentUrl = new URL(window.location.href);
+//   let navPath, footerPath;
 
-  if (currentUrl.protocol == "https:") {
-    if (currentUrl.pathname === '/' || currentUrl.pathname.endsWith('/index.html')) {
-      console.log("helo deploi");
+//   if (currentUrl.protocol == "https:") {
+//     if (currentUrl.pathname === '/' || currentUrl.pathname.endsWith('/index.html')) {
+//       console.log("helo deploi");
       
-      navPath = '../assets/partials/navbar.html';
-      footerPath = '../assets/partials/footer.html';
-    } else {
-            console.log("helo deploi aver");
-      navPath = '../../assets/partials/navbar.html';
-      footerPath = '../../assets/partials/footer.html';
-    }
-  }
+//       navPath = '../assets/partials/navbar.html';
+//       footerPath = '../assets/partials/footer.html';
+//     } else {
+//             console.log("helo deploi aver");
+//       navPath = '../../assets/partials/navbar.html';
+//       footerPath = '../../assets/partials/footer.html';
+//     }
+//   }
 
-  if (currentUrl.pathname === '/' || currentUrl.pathname.endsWith('/index.html')) {
-    navPath = './assets/partials/navbar.html';
-    footerPath = './assets/partials/footer.html';
-  } else {
-    navPath = '../assets/partials/navbar.html';
-    footerPath = '../assets/partials/footer.html';
-  }
+//   if (currentUrl.pathname === '/' || currentUrl.pathname.endsWith('/index.html')) {
+//     navPath = './assets/partials/navbar.html';
+//     footerPath = './assets/partials/footer.html';
+//   } else {
+//     navPath = '../assets/partials/navbar.html';
+//     footerPath = '../assets/partials/footer.html';
+//   }
 
-  loadPartial('navbar-placeholder', navPath);
-  loadPartial('footer-placeholder', footerPath);
-});
+//   loadPartial('navbar-placeholder', navPath);
+//   loadPartial('footer-placeholder', footerPath);
+// });
+
+const currentUrl = new URL(window.location.href);
+console.log(currentUrl.protocol); // "https:"
+console.log(currentUrl.host);     // "example.com"
+console.log(currentUrl.pathname); // "/page"
+console.log(currentUrl.search);   // "?id=123"
+console.log(currentUrl.hash);     // "#section2"
