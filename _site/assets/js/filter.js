5c38f4c2f7e67bb9f8ee96f2a7d913f565062703
el <eh.benaboud.dev@gmail.com>
2026-01-25 22:19:49 +0100

    g

--- _site/assets/js/filter.js
+++ _site/assets/js/filter.js
@@ -1,20 +1,26 @@
-const buttons = document.querySelectorAll('[data-filter]')
-const skills = document.querySelectorAll('[data-category]')
+const buttons = document.querySelectorAll('[data-filter]');
+const skills = document.querySelectorAll('[data-category]');
 
 buttons.forEach(btn => {
   btn.addEventListener('click', () => {
-    const filterValue = btn.dataset.filter
+    const filterValue = btn.dataset.filter;
 
     buttons.forEach(b => b.classList.remove('active'));
     btn.classList.add('active');
 
+    console.log(btn.dataset.filter);
+
     skills.forEach(element => {
       element.style.display = 'block';
+
       if (filterValue !== "all") {
-        if (element.dataset.category !== filterValue) {
+        // prendi tutte le categorie dell'elemento e crea un array
+        const categories = element.dataset.category.split(',').map(c => c.trim());
+        // se l'array non include il filtro, nascondi
+        if (!categories.includes(filterValue)) {
           element.style.display = 'none';
         }
       }
     });
-  })
-})
+  });
+});
